90e78fc8aa1a9f19d9e4fac06c4a2a91815f4435
VISHAL SINGHAL <ivishalsinghal@gmail.com>
2019-02-21 09:31:28 +0530

    u

--- src/main/java/com/tcs/TestController.java
+++ src/main/java/com/tcs/TestController.java
@@ -1,6 +1,6 @@
 package com.tcs;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
